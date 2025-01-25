8dd9c5591a627e53859e6014e8b1095e9ae8dd99
Nathalia Adriele <nat-adriele@hotmail.com>
2025-01-25 16:46:42 +0000

    op

--- 01-docker-terraform/ingest_data_zone.py
+++ 01-docker-terraform/ingest_data_zone.py
@@ -1,4 +1,3 @@
-
 import os
 import argparse
 
@@ -14,7 +13,7 @@ def main(params):
     db = params.db
     table_name = params.table_name
     url = params.url
-
+    
     if url.endswith('.csv.gz'):
         csv_name = 'output_zone.csv.gz'
     else:
@@ -38,6 +37,7 @@ if __name__ == '__main__':
     parser.add_argument('--host', required=True, help='host for postgres')
     parser.add_argument('--port', required=True, help='port for postgres')
     parser.add_argument('--db', required=True, help='database name for postgres')
+    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
     parser.add_argument('--url', required=True, help='url of the csv file')
 
     args = parser.parse_args()
