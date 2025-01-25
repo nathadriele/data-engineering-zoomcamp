9091f3e9f2c5d4d051655a19f30418ea87ba9d5c
Nathalia Adriele <nat-adriele@hotmail.com>
2025-01-25 16:45:26 +0000

    ingest

---
+++ 01-docker-terraform/ingest_data_zone.py
@@ -0,0 +1,40 @@
+import os
+import argparse
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host 
+    port = params.port 
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+    
+    if url.endswith('.csv.gz'):
+        csv_name = 'output_zone.csv.gz'
+    else:
+        csv_name = 'output_zone.csv'
+
+    os.system(f"wget {url} -O {csv_name}")
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    df = pd.read_csv(csv_name) 
+    
+    print("Finished ingesting data into the postgres database")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+    parser.add_argument('--user', required=True, help='user name for postgres')
+    parser.add_argument('--host', required=True, help='host for postgres')
+    parser.add_argument('--db', required=True, help='database name for postgres')
+    parser.add_argument('--url', required=True, help='url of the csv file')
+
+    args = parser.parse_args()
+
+    main(args)
