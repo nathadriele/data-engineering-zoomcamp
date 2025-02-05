06288d93b1d4972952d8eca3c733c8969d9d6dc8
Nathalia Adriele <nat-adriele@hotmail.com>
2025-02-05 13:46:44 +0000

    query

---
+++ 03-data-warehouse/queries/create_green_table.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS green_trip (
+    vendorid smallint,
+    lpep_pickup_datetime timestamp NOT NULL,
+    lpep_dropoff_datetime timestamp NOT NULL,
+    store_and_fwd_flag boolean,
+    ratecodeid smallint,
+    pulocationid int,
+    dolocationid int,
+    passenger_count smallint,
+    trip_distance numeric,
+    fare_amount numeric,
+    extra numeric,
+    mta_tax numeric,
+    tip_amount numeric,
+    tolls_amount numeric,
+    ehail_fee numeric,
+    improvement_surcharge numeric,
+    total_amount numeric,
+    payment_type smallint,
+    trip_type smallint,
+    congestion_surcharge numeric,
+    CONSTRAINT trip_unique UNIQUE (
+        lpep_pickup_datetime,
+    )
+);
