ea445cd7fee6fc19e0b6f160b6cc4226228c4742
Nathalia Adriele <nat-adriele@hotmail.com>
2025-02-05 13:42:39 +0000

    resource

--- 03-data-warehouse/main.tf
+++ 03-data-warehouse/main.tf
@@ -17,3 +17,18 @@ resource "google_storage_bucket" "zoomcamp_week_3_bucket" {
   name          = var.gcs_bucket_name
   location      = var.location
   force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = ""
+    }
+  }
+}
+
+resource "" "" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+}
