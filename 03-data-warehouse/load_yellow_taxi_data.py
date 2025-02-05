8b8b9ec6b76cf694a2bc3c126275d6578402418f
Nathalia Adriele <nat-adriele@hotmail.com>
2025-02-05 13:38:20 +0000

    adjs

--- 03-data-warehouse/load_yellow_taxi_data.py
+++ 03-data-warehouse/load_yellow_taxi_data.py
@@ -1,71 +1,43 @@
 import os
-import urllib.request
+import tempfile
 from concurrent.futures import ThreadPoolExecutor
+
+import requests
 from google.cloud import storage
-import time
+from dotenv import load_dotenv
 
-BUCKET_NAME = "zoomcamp-week-3-bucket"  
+load_dotenv()
 
-CREDENTIALS_FILE = "/home/marc/data_engineering_zoomcamp_2025/3_data_warehouse/secrets/zoomcamp-week-3-9cfd78e84128.json"  
-client = storage.Client.from_service_account_json(CREDENTIALS_FILE)
+# Change this to your bucket name
+BUCKET_NAME = os.environ["BUCKET_NAME"]
+
+# If you authenticated through the GCP SDK you can comment out these two lines
+# CREDENTIALS_FILE = "gcs.json"
+# client = storage.Client.from_service_account_json(CREDENTIALS_FILE)
+
 
 BASE_URL = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-"
-MONTHS = [f"{i:02d}" for i in range(1, 7)] 
-DOWNLOAD_DIR = "."
+MONTHS = [f"{i:02d}" for i in range(1, 7)]
 
-CHUNK_SIZE = 8 * 1024 * 1024  
 
-os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+def transfer_file(month):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(BUCKET_NAME)
+    file_name = f"yellow_tripdata_2024-{month}.parquet"
+    blob = bucket.blob(file_name)
 
-bucket = client.bucket(BUCKET_NAME)
-
-def download_file(month):
     url = f"{BASE_URL}{month}.parquet"
-    file_path = os.path.join(DOWNLOAD_DIR, f"yellow_tripdata_2024-{month}.parquet")
-
-    try:
-        print(f"Downloading {url}...")
-        urllib.request.urlretrieve(url, file_path)
-        print(f"Downloaded: {file_path}")
-        return file_path
-    except Exception as e:
-        print(f"Failed to download {url}: {e}")
-        return None
-
-
-def verify_gcs_upload(blob_name):
-    return storage.Blob(bucket=bucket, name=blob_name).exists(client)
-
-
-def upload_to_gcs(file_path, max_retries=3):
-    blob_name = os.path.basename(file_path)
-    blob = bucket.blob(blob_name)
-    blob.chunk_size = CHUNK_SIZE  
-    
-    for attempt in range(max_retries):
-        try:
-            print(f"Uploading {file_path} to {BUCKET_NAME} (Attempt {attempt + 1})...")
-            blob.upload_from_filename(file_path)
-            print(f"Uploaded: gs://{BUCKET_NAME}/{blob_name}")
-            
-            if verify_gcs_upload(blob_name):
-                print(f"Verification successful for {blob_name}")
-                return
-            else:
-                print(f"Verification failed for {blob_name}, retrying...")
-        except Exception as e:
-            print(f"Failed to upload {file_path} to GCS: {e}")
-        
-        time.sleep(5)  
-    
-    print(f"Giving up on {file_path} after {max_retries} attempts.")
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()
+        with tempfile.TemporaryFile() as file:
+            for chunk in response.iter_content(1024 * 8):
+                file.write(chunk)
+            blob.upload_from_file(file, rewind=True)
+    print(f"uploaded {month}.")
 
 
 if __name__ == "__main__":
     with ThreadPoolExecutor(max_workers=4) as executor:
-        file_paths = list(executor.map(download_file, MONTHS))
-
-    with ThreadPoolExecutor(max_workers=4) as executor:
-        executor.map(upload_to_gcs, filter(None, file_paths))
+        file_paths = list(executor.map(transfer_file, MONTHS))
 
     print("All files processed and verified.")
