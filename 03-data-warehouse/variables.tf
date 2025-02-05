0af0c1291b6057cbf3726c72ed65deea1040264f
Nathalia Adriele <nat-adriele@hotmail.com>
2025-02-05 13:39:32 +0000

    region

---
+++ 03-data-warehouse/variables.tf
@@ -0,0 +1,21 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "./secrets/zoomcamp-week-3-9cfd78e84128.json"
+}
+
+variable "project" {
+  description = "Project"
+  default     = "zoomcamp-week-3"
+
+}
+
+variable "region" {
+  description = "Region"
+  default     = "us-central1"
+
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "US"
+}
