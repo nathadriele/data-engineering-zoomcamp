be42dffba8460e672bfc6f3acf7f2d8a449f82be
Nathalia Adriele <nat-adriele@hotmail.com>
2025-02-06 18:29:35 +0000

    read url data

--- 03-data-warehouse/scripts/ingest_url_data.py
+++ 03-data-warehouse/scripts/ingest_url_data.py
@@ -82,3 +82,56 @@ def get_taxi_data(color="green", year=2019, month=1):
                 % (color, year, month)
             )
             return
+
+        create_table(conn, color)
+        import_data(data, conn, TABLES[color])
+        data.close()
+        mark_as_inserted(conn, color, year, month)
+        conn.commit()
+        print("Transaction complete")
+
+
+def create_table(conn, color: str):
+    cursor = conn.cursor()
+    cursor.execute(CREATE_TAXI_TABLE[color])
+
+
+def already_inserted(conn, color: str, year: int, month: int) -> bool:
+    cursor = conn.cursor()
+    cursor.execute(CREATE_INSERTED_TABLE)
+    cursor.execute(
+        f"select 1 from {INSERTED_DATA} where color='{color}' AND year={year} AND month={month}"
+    )
+    return cursor.fetchone() is not None
+
+
+def mark_as_inserted(conn, color: str, year: int, month: int) -> None:
+    cursor = conn.cursor()
+    cursor.execute(
+        f"insert into {INSERTED_DATA}(color, year, month) VALUES('{color}', {year}, {month})"
+    )
+    print(f"inserted {color}, {year}, {month}")
+
+
+def create_connection():
+    user = os.getenv("DB_USER", "postgres")
+    password = os.getenv("DB_PASSWORD", "example")
+    host = os.getenv("DB_HOST", "localhost")
+    port = os.getenv("DB_PORT", "5432")
+    db = os.getenv("DB_DB", "taxi")
+
+    return psycopg2.connect(f"postgresql://{user}:{password}@{host}:{port}/{db}")
+
+
+def read_url_data(color: str, year: int, month: int):
+    temp_file = tempfile.TemporaryFile()
+    assert 1 <= month <= 12
+    assert color in TABLES.keys()
+    url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{color}_tripdata_{year:04d}-{month:02d}.csv.gz"
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+    for chunk in response.iter_content(chunk_size=1024):
+        temp_file.write(chunk)
+    print("downloaded data from %s into %s" % (url, temp_file.name))
+    temp_file.seek(0) 
+    return temp_file
