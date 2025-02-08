1d0b3d4237d6c61e9f06b40ba2b555a86961a14e
Nathalia Adriele <nat-adriele@hotmail.com>
2025-02-08 17:50:05 +0000

    add

---
+++ 03-data-warehouse/scripts/load-data-to-db.py
@@ -0,0 +1,75 @@
+import argparse
+import os
+import sys
+from time import time
+
+import pandas as pd
+import pyarrow.parquet as pq
+from sqlalchemy import create_engine
+
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    tb = params.tb
+    file: str = params.file
+
+    if file.startswith("http"):
+        file_name = url.rsplit("/", 1)[-1].strip()
+        print(f"Downloading {file_name} ...")
+        os.system(f"curl {url.strip()} -o {file_name}")
+    else:
+        file_name = file
+
+    engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
+
+    if ".csv" in file_name:
+        df = pd.read_csv(file_name, nrows=10)
+        df_iter = pd.read_csv(file_name, iterator=True, chunksize=100000)
+    elif ".parquet" in file_name:
+        file = pq.ParquetFile(file_name)
+        df = next(file.iter_batches(batch_size=10)).to_pandas()
+    else:
+        print("Error. Only .csv or .parquet files allowed.")
+        sys.exit()
+
+    df.head(0).to_sql(name=tb, con=engine, if_exists="replace")
+
+    t_start = time()
+    count = 0
+    for batch in df_iter:
+        count += 1
+
+        if ".parquet" in file_name:
+            batch_df = batch.to_pandas()
+        else:
+            batch_df = batch
+
+        print(f"inserting batch {count}...")
+
+        b_start = time()
+        batch_df.to_sql(name=tb, con=engine, if_exists="append")
+
+        print(f"inserted! time taken {b_end-b_start:10.3f} seconds.\n")
+
+    t_end = time()
+    print(
+        f"Completed! Total time taken was {t_end-t_start:10.3f} seconds for {count} batches."
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Loading data from .paraquet file link to a Postgres datebase."
+    )
+
+    parser.add_argument("--user", help="Username for Postgres.")
+    parser.add_argument("--password", help="Password to the username for Postgres.")
+    parser.add_argument("--port", help="Port for Postgres connection.")
+    parser.add_argument("--tb", help="Destination table name for Postgres.")
+
+    args = parser.parse_args()
+    main(args)
